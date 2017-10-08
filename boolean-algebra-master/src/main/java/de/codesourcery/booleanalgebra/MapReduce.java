34555a4f501c48911044b20bba09397861942dc1
Rohit Nambiar <rnambi2@uic.edu>
2017-10-08 00:38:09 -0500

    finished sorting MapReduce

--- boolean-algebra-master/src/main/java/de/codesourcery/booleanalgebra/MapReduce.java
+++ boolean-algebra-master/src/main/java/de/codesourcery/booleanalgebra/MapReduce.java
@@ -7,22 +7,28 @@ import org.apache.hadoop.util.*;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class MapReduce {
+	public static HashMap<String, Integer> testLineCount = new HashMap<String, Integer>();
 
 	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
 		private Text testName = new Text();
 		private Text lineNoAndClass = new Text();
 
-		public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) 
-		throws IOException {
+		public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter)
+				throws IOException {
 			String line = value.toString();
 			StringTokenizer tokenizer = new StringTokenizer(line);
 			if (tokenizer.hasMoreTokens()) {
-				lineNoAndClass.set(tokenizer.nextToken() + " : " + tokenizer.nextToken());
+				lineNoAndClass.set("<" + tokenizer.nextToken() + ":" + tokenizer.nextToken() + ", ");
 			}
 			if (tokenizer.hasMoreTokens()) {
-				testName.set(tokenizer.nextToken());
+				String test = tokenizer.nextToken();
+				testLineCount.put(test,
+						testLineCount.getOrDefault(test, 0) + 1);
+				testName.set(test);
 			}
 			output.collect(lineNoAndClass, testName);
 		}
@@ -30,15 +36,44 @@ public class MapReduce {
 
 	public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
 		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter)
-		throws IOException {
-			StringBuilder tests = new StringBuilder("<");
+				throws IOException {
+			StringBuilder tests = new StringBuilder("(");
+			ArrayList<String> testNameList = new ArrayList<String>();
 			while (values.hasNext()) {
-				tests.append(values.next() + ", ");
+				testNameList.add(values.next().toString());
+			}
+			testNameList = sortTestNameList(testNameList);
+			for (String testName: testNameList) {
+				tests.append(testName + ", ");
 			}
 			tests.delete(tests.length()-2, tests.length());
-			tests.append(">");
+			tests.append(")>");
 			output.collect(key, new Text(tests.toString()));
 		}
+	}
+
+	public static ArrayList<String> sortTestNameList(ArrayList<String> testNameList) {
+		System.out.println("setNullTest count: " +
+				testLineCount.get("setNullTest"));
+		System.out.println("removeNullTest count: " +
+				testLineCount.get("removeNullTest"));
+		System.out.println("lookupNullTest count: " +
+				testLineCount.get("lookupNullTest"));
+		if (testNameList.size() < 2) return testNameList;
+		boolean sorted = true;
+		while (sorted) {
+			sorted = false;
+			for (int i = 1; i < testNameList.size(); i++) {
+				if (testLineCount.get(testNameList.get(i)) >
+						testLineCount.get(testNameList.get(i-1))) {
+					sorted = true;
+					String temp = testNameList.get(i);
+					testNameList.set(i, testNameList.get(i-1));
+					testNameList.set(i-1, temp);
+				}
+			}
+		}
+		return testNameList;
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -47,7 +82,6 @@ public class MapReduce {
 		conf.setOutputKeyClass(Text.class);
 		conf.setOutputValueClass(Text.class);
 		conf.setMapperClass(Map.class);
-		conf.setCombinerClass(Reduce.class);
 		conf.setReducerClass(Reduce.class);
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
